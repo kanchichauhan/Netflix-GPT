a53790e6e2b4003524c6c1bc2dd44c5b5c61a331
kanchichauhan <k.chauhan27199@gmail.com>
2024-05-12 01:06:54 +0530

    sign-up/sign-in login box added

--- netflix_gpt/src/components/Login.js
+++ netflix_gpt/src/components/Login.js
@@ -1,8 +1,27 @@
 import Header from "./Header";
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [isSignIn, setSignInState] = useState(true);
+
+    const handleSignInState = () => {
+        setSignInState(!isSignIn);
+    }
     return (
-    <div><Header /></div>
+    <div>
+        <div><Header /></div>
+        <form>
+            <h1>{!isSignIn ? 'Sign up' : 'Sign in'}</h1>
+            {(!isSignIn) && <input type="text" placeholder="Fullname" class="fullname"/>}
+            <input type="text" placeholder="Email Address" class="email"/>
+            <input type="password" placeholder="Password" class="password"/>
+            <button class="sign-in-button">
+                {!isSignIn ? 'Sign up' : 'Sign in'}</button>
+            <p onClick={handleSignInState}>
+                {!isSignIn ? 'Already Registered? Sign In now' : 'New to Netflix? Sign up now!'}</p>
+        </form>
+    </div>
     )
 };
 export default Login;
