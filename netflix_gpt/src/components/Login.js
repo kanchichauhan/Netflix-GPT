5e455a74db3e2458b7aa6fd348f35fd603adba48
kanchichauhan <k.chauhan27199@gmail.com>
2024-05-24 10:54:03 +0530

    slice added

--- netflix_gpt/src/components/Login.js
+++ netflix_gpt/src/components/Login.js
@@ -1,13 +1,15 @@
 import Header from "./Header";
 import { useState, useRef } from 'react';
 import { formValidation } from "../utils/formValidation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
     const [isSignIn, setSignInState] = useState(true);
     const [error, setError] = useState(false);
+    const navigate = useNavigate();
 
     const email = useRef(null);
     const password = useRef(null);
@@ -33,6 +35,21 @@ const Login = () => {
                     // Signed up 
                     const user = userCredential.user;
                     console.log(user);
+                    navigate('/browse')
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setError(errorCode +'-'+errorMessage)
+                });
+            } else {
+                signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    // Signed up 
+                    const user = userCredential.user;
+                    console.log(user);
+                    navigate('/browse')
                     // ...
                 })
                 .catch((error) => {
@@ -46,7 +63,11 @@ const Login = () => {
 
     return (
     <div>
-        <div><Header /></div>
+        <div><Header />
+        <div className="netflix-background">
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+                <div className="netflix-overlay"></div>
+            </div></div>
         <form className="signin-form">
             <h1>{!isSignIn ? 'Sign up' : 'Sign in'}</h1>
             {(!isSignIn) && <input ref={fullname} type="text" placeholder="Fullname" className="fullname"/>}
