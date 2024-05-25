958f54dcd1867a4354b0d74065a17393fa6fc8c8
kanchichauhan <k.chauhan27199@gmail.com>
2024-05-26 01:04:48 +0530

    login done

--- netflix_gpt/src/components/Header.js
+++ netflix_gpt/src/components/Header.js
@@ -1,8 +1,27 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+
 const Header = () => {
+    const navigate = useNavigate();
+    const user = useSelector((store) => store.user);
+    const handleSignout = () => {
+        signOut(auth).then(() => {
+            navigate("/");
+        }).catch((error) => {
+            navigate("/error");
+        });
+    };
+
     return (
-        <div>
+        <><div>
             <div className="netflix-logo"><img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" /></div>
         </div>
+        {user && (<div className='signedin-header'><span>{user.displayName}</span>
+                <img src="https://occ-0-3752-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABb7kuX9mKPrFGfvZ0oJ9eMBbFCB7ZhumT7uHIoILp1FtGpeIhybv8zoGgNK76rr7N8bMdhn-kkbRnD6ut8mFLwqYXmdpwCw.png?r=eea"/>
+                <button onClick={handleSignout} className="signout-btn">Sign out</button>
+            </div>)}</>
     )
 };
 export default Header;
