7dcf5263ffeac756f3e7389b6d6e8153206b3f1f
kanchichauhan <k.chauhan27199@gmail.com>
2024-05-30 23:40:04 +0530

    TMDB API added to browse page

--- netflix_gpt/src/components/Browse.js
+++ netflix_gpt/src/components/Browse.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import { options } from "./constants";
 
 const Browse = () => {
+
+    const  recentPlayingMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        const json = await data.json();
+        console.log(json.results);
+    }
+    useEffect(() => {
+        recentPlayingMovies();
+    }, [])
+
     return <div>
         <Header />
     </div>
--- netflix_gpt/src/components/constants.js
+++ netflix_gpt/src/components/constants.js
@@ -1,3 +1,10 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const userAvatar = "https://occ-0-3752-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABb7kuX9mKPrFGfvZ0oJ9eMBbFCB7ZhumT7uHIoILp1FtGpeIhybv8zoGgNK76rr7N8bMdhn-kkbRnD6ut8mFLwqYXmdpwCw.png?r=eea";
 export const netflixBack = "https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+export const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmM0OTVmY2ZjY2YwODI5ZTFjYjIxN2IwOWMzNjE1OCIsInN1YiI6IjY2NThhYzZhMzYyZWY4OGRkZWE2MjAzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5NvYKGh-4Q3wJpeX8hv7Kct577J0NwunSN46jhHVWn4'
+    }
+};
