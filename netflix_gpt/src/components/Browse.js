f8452ca9fc2d0623fb04cead9fd581d1e4edc1a0
kanchichauhan <k.chauhan27199@gmail.com>
2024-05-31 12:31:39 +0530

    TMDB API added to browse page

--- netflix_gpt/src/components/Browse.js
+++ netflix_gpt/src/components/Browse.js
@@ -1,13 +1,17 @@
 import { useEffect } from "react";
 import Header from "./Header";
 import { options } from "./constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
 
 const Browse = () => {
+    const dispatch = useDispatch();
 
     const  recentPlayingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
         const json = await data.json();
         console.log(json.results);
+        dispatch(addNowPlayingMovies(json.results))
     }
     useEffect(() => {
         recentPlayingMovies();
